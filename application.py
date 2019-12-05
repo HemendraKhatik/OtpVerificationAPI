fc5801f5b4c7624990d807521524a4c213fecfc5
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2019-12-05 17:03:42 +0530

    generating random six digits otp and returning as a json

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
 
 @app.route("/otp_varification/<int:phone_number>",methods=['get'])
 def otp_varification(phone_number):
-	return "route is working"
+	# generating six digits otp
+	otp = random.randrange(100000,999999)
+	return jsonify(otp= otp)
 
 if __name__ == "__main__":
     app.run()   
