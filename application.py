19d49d6917822d195b2225c441dc0cde14844310
Hemendra Khatik <hemendra.khatik010698@gmail.com>
2019-12-05 17:17:59 +0530

    Added Way2sms API to send otp to the given number

--- application.py
+++ application.py
@@ -1,13 +1,37 @@
 from flask import Flask, jsonify
 import random
+import requests
+import json
+import random 
 
 app = Flask(__name__)
 
-@app.route("/otp_varification/<int:phone_number>",methods=['get'])
-def otp_varification(phone_number):
+@app.route("/otp_varification/<int:receiver_phone_number>",methods=['get'])
+def otp_varification(receiver_phone_number):
 	# generating six digits otp
 	otp = random.randrange(100000,999999)
+	api_key = "QSMJHMHYHX9C6SZDWMY5OIT14M06238F"
+	secret_key = "0ZFVPHPK9QDTQ04U"
+	usetype = "stage"
+	sender_email = "hemendra.khatik010698@gmail.com"
+	otp_message = "FININD verification your otp is "+ str(otp)
+	sendGetRequest(URL, api_key, secret_key, usetype, receiver_phone_number, sender_email, otp_message)
 	return jsonify(otp= otp)
+
+
+URL = 'https://www.way2sms.com/api/v1/sendCampaign'
+
+# get request
+def sendGetRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textMessage):
+  req_params = {
+  'apikey':apiKey,
+  'secret':secretKey,
+  'usetype':useType,
+  'phone': phoneNo,
+  'message':textMessage,
+  'senderid':senderId
+  }
+  return requests.get(reqUrl, req_params)    	
 
 if __name__ == "__main__":
     app.run()   
